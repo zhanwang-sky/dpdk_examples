92b5225db72eb61e31077e16ad2e0d829ef3cc50
zhanwang-sky <zhanwang-sky@users.noreply.github.com>
2025-02-13 20:48:01 +0800

    simple_mp

---
+++ multi_process/simple_mp/main.c
@@ -0,0 +1,13 @@
+//
+//  main.c
+//  dpdk_examples
+//
+//  Created by zhanwang-sky on 2025/2/13.
+//
+
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+    printf("Hello World!\n");
+    return 0;
+}
